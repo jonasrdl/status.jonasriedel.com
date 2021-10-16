bbc6d488eb06a30130ca2f35ebbac0bc44b38a4e
Jonas <jonasriedel@protonmail.com>
2021-10-16 22:03:49 +0200

    Change display functionality

--- src/components/JonasDiscordBot/JonasDiscordBot.jsx
+++ src/components/JonasDiscordBot/JonasDiscordBot.jsx
@@ -1,4 +1,57 @@
-const API = `http://status.jonasriedel.com/api`
+import React from "react"
+
+class JonasDiscordBot extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      onlineData: [],
+      DataisLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://status.jonasriedel.com/api/jonasdiscordbot')
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          onlineData: json,
+          DataisLoaded: true
+        })
+      })
+  }
+
+  render() {
+    const { DataisLoaded, onlineData } = this.state
+
+    if (!DataisLoaded) {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {
+          onlineData.map((item) => (
+            <p>{item}</p>
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default JonasDiscordBot 
+
+
+
+
+
+
+/* const API = `http://status.jonasriedel.com/api`
 const axios = require('axios')
 
 const isOnline = (callback) => {
@@ -9,15 +62,19 @@ const isOnline = (callback) => {
   })
 }
 
-const JonasDiscordBot = () => {
+const JonasDiscordBot = (props) => {
   return (
     <>
       <p>JonasDiscordBot</p>
-      <p>{ isOnline((websiteOnline) => {
+
+      <p>
+      { isOnline((websiteOnline) => {
         websiteOnline
-      }) }</p>
+      }) }
+      </p>
+
+      <p>{props.content}</p>
     </>
   )
 }
-
-export default JonasDiscordBot
+*/
